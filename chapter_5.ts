19f7c9e0fdd4d0c188e3f6edf5cf95762a44d7c3
charlesanthony1996 <charlesanthony296@gmail.com>
2025-09-02 11:35:21 +0200

    xml http request

--- chapter_5.ts
+++ chapter_5.ts
@@ -115,6 +115,263 @@ lemur()
 
 // listing 5-10 using apply
 
-function go(callback: (arg: string) => void) {
+// function go(callback: (arg: string) => void) {
+//     callback.apply(callbackFunction, ['Example argument'])
+// }
 
+// function callbackFunction(arg: string) {
+//     // alert(arg)
+//     console.log(arg)
+// }
+
+// go(callbackFunction)
+
+// listing 5-11 simple function call 
+
+function go(callback: (arg: string) => void) {
+    callback('Example argument')
 }
+
+function callbackFunction(arg: string) {
+    // alert(arg)
+    console.log(arg)
+}
+
+go(callbackFunction)
+
+// listing 5-12 using apply to convert array to arguments
+
+const numbers = [3, 11, 5, 7, 2, 18]
+
+// a fragile way of finding the maximum number
+// const max = Math.max(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4])
+
+// a solid way to find the maximum
+const max = Math.max.apply(null, numbers)
+
+console.log(max)
+
+// listing 5-13 simple observer
+
+interface Subscriber {
+    (message: string): void
+}
+
+class Publisher {
+    private subscribers: Subscriber[] = []
+
+    addSubscriber(subscriber: Subscriber) {
+        this.subscribers.push(subscriber)
+    }
+
+    notify(message: string) {
+        for (let subscriber of this.subscribers) {
+            subscriber(message)
+        }
+    }
+}
+
+const publisher = new Publisher()
+
+// using an arrow function
+publisher.addSubscriber((message) => console.log('A: ' + message))
+
+// using an inline function
+publisher.addSubscriber(function (message) {
+    console.log('B: ' + message)
+})
+
+publisher.notify('Test message')
+
+// listing 5-14 fictitious api
+
+interface fictitiousData {
+    id: number
+    name: string
+}
+
+class fictitiousAPI {
+    static data: { [index: number]: fictitiousData} = {
+        1: { id: 1, name: 'Aramis'},
+        2: { id: 2, name: 'Athos'},
+        3: { id: 3, name: 'Porthos'},
+        4: { id: 4, name: 'D\Artagnan'}
+    }
+
+    static getData(id: number, callback: (data: fictitiousData) => void) {
+        // simulating async data access with a timeout
+        setTimeout(() => {
+            const result = this.data[id]
+
+            if (typeof result == 'undefined') {
+                throw new Error('No matching record')
+            }
+            callback(result)
+        }, 200)
+    }
+}
+
+// listing 5-15 single call
+
+fictitiousAPI.getData(1, function(data) {
+    console.log(data.name)
+})
+
+// listing 5-16 error handling
+
+try {
+    fictitiousAPI.getData(4, function(data) {
+        console.log(data.name)
+    })
+} catch (ex) {
+    console.log('this statement is not reached, the error is not caught')
+}
+
+// listing 5-17 nested callbacks
+
+fictitiousAPI.getData(1, (data) => {
+    console.log(data.name)
+
+    fictitiousAPI.getData(2, (data) => {
+        if (data.name == 'Athos') {
+            console.log(data.id + ' ' + data.name)
+        } else {
+            console.log(data.name)
+        }
+
+        fictitiousAPI.getData(3, (data) => {
+            console.log(data.name)
+
+            fictitiousAPI.getData(4, (data) => {
+                console.log(data.name)
+
+                // fictitiousAPI.getData(5, (data) => {
+                //     console.log(data.name)
+                // })
+            })
+        })
+    })
+})
+
+// callbacks and error handling
+
+// listing 5-18 fictitious api 2.0 
+
+interface fictitiousData2 {
+    id: number
+    name: string
+}
+
+// class FictitiousAPI {
+//     static data: { [index: number]: fictitiousData2 } = {
+//         1: { id: 1, name: 'Aramis'},
+//         2: { id: 2, name: 'Athos'},
+//         3: { id: 3, name: 'Porthos'},
+//         4: { id: 4, name: 'D\'Artagnan' },
+//     }
+
+//     static getData(id: number, callback: (error: string, data: fictitiousData2) => void) {
+//         // simulating async data access with a timeout
+//         window.setTimeout(() => {
+//             const result = this.data[id]
+
+//             if (typeof result == 'undefined') {
+//                 callback('No matching record', null)
+//                 return
+//             }
+
+//             callback(null, result)
+//         }, 200)
+//     }
+// }
+
+// listing 5-19 single call with error handling
+
+// single call: Aramis
+
+// fictitiousAPI.getData(1, function (error, data) {
+//     if (error) {
+//         console.log('Caught: ' + error)
+//         return
+//     }
+//     console.log(data.name)
+// })
+
+// listing 5-22 fictitious api v3.0
+
+interface fictitiousdata3 {
+    id: number
+    name: string
+}
+
+class fictitiousapi3 {
+    static data: { [index: number]: fictitiousdata3} = {
+        1: { id: 1, name: 'Aramis'},
+        2: { id: 2, name: 'Athos'},
+        3: { id: 3, name: 'Porthos'},
+        4: { id: 4, name: 'D\'Artagnan' }
+    }
+
+    static getData(id: number) {
+        return new Promise((fulfil: (data: fictitiousdata3) => void, reject: (reason: string) => void) => {
+            // simulating async data access with a timeout
+            setTimeout(() => {
+                const result = this.data[id]
+
+                if (typeof result == 'undefined') {
+                    reject(('No matching record'))
+                }
+
+                fulfil(result)
+            }, 200)
+        })
+    }
+}
+
+// listing 5-23 single call with then
+
+// single call aramis
+fictitiousapi3.getData(1)
+.then(function (data) {
+    console.log(data.name)
+})
+
+fictitiousapi3.getData(5)
+.then(function (data) {
+    console.log(data.name)
+})
+.catch(function (error) {
+    console.log('Caught ' + error)
+})
+
+// listing 5-26 promise all
+
+Promise.all([
+    fictitiousapi3.getData(1),
+    fictitiousapi3.getData(2),
+    fictitiousapi3.getData(3),
+    fictitiousapi3.getData(4),
+    // fictitiousapi3.getData(1),
+]).then((values) => {
+    for (let val of values) {
+        console.log(val.name)
+    }
+}).catch((error) => {
+    console.log('Caught: ' + error)
+})
+
+// listing 5-27 fastest promise
+
+Promise.race([
+    fictitiousapi3.getData(1),
+    fictitiousapi3.getData(2),
+    fictitiousapi3.getData(3),
+    fictitiousapi3.getData(4),
+]).then((data) => {
+    console.log(data.name)
+}).catch((error) => {
+    console.log('caught: ' + error)
+})
+
+
+// listing 5-28 xml http request
